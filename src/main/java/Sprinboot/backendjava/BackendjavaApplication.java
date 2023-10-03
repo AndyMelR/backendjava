ffdab51c4bd67ec301d8a82a582bfc313895b44d
Andrea Melero <ameleror@gmail.com>
2023-10-03 19:36:43 +0200

    Creacion primer controller

---
+++ src/main/java/Sprinboot/backendjava/BackendjavaApplication.java
@@ -0,0 +1,14 @@
+package Sprinboot.backendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendjavaApplication.class, args);
+		System.out.print("Funcionando");
+	}
+
+}
---
+++ src/main/java/Sprinboot/backendjava/controllers/UserController.java
@@ -0,0 +1,10 @@
+package Sprinboot.backendjava.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")//localhost:8080/users prefijo que le especificamos en la anotacion
+public class UserController {
+    
+}
