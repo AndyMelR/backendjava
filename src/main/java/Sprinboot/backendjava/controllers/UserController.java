87bcdfe61ed3fcb3a489fabb96f02faaec3b21c0
Andrea Melero <ameleror@gmail.com>
2023-10-04 15:55:01 +0200

    added methods

--- src/main/java/Sprinboot/backendjava/controllers/UserController.java
+++ src/main/java/Sprinboot/backendjava/controllers/UserController.java
@@ -1,10 +1,23 @@
 package Sprinboot.backendjava.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")//localhost:8080/users prefijo que le especificamos en la anotacion
 public class UserController {
+
+    @GetMapping
+    public String getUser() {
+        return "get user details";
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "create new user";
+
+    }
     
 }
